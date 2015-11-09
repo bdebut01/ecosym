be95f2cf29c7f0525bb5bb060e59c22180bd6622
bdebut01 <ben@debutts.com>
2015-11-09 15:31:06 -0500

    arg fix

--- SeaBlock.py
+++ SeaBlock.py
@@ -1,9 +1,9 @@
 class SeaBlock :
 	def __init__(self, salinity = 1, sun = 0, oxygen = 0, pressure = 0) :
-		self.__salinity  = sal
+		self.__salinity  = salinity
 		self.__sunlight  = sun
-		self.__oxygen  	 = oxy
-		self.__pressure  = press
+		self.__oxygen  	 = oxygen
+		self.__pressure  = pressure
 		self.__organisms = {}
 
 	def removeOrganism(id) :
