98d00a8ece0835b8a8d380995019d5e761e6147b
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-11-09 11:44:35 -0500

    Fix some typos

--- SeaBlock.py
+++ SeaBlock.py
@@ -1,16 +1,16 @@
 class SeaBlock :
 	def __init__(self, salinity = 1, sun = 0, oxygen = 0, pressure = 0) :
-		self.__salinity  = sal
+		self.__salinity  = salinity
 		self.__sunlight  = sun
-		self.__oxygen  	 = oxy
-		self.__pressure  = press
+		self.__oxygen  	 = oxygen
+		self.__pressure  = pressure
 		self.__organisms = {}
 
-	def removeOrganism(id) :
+	def removeOrganism(self, id) :
 		del self.__organisms[id]
 
-	def addOrganism(id, Creature) :
+	def addOrganism(self, id, Creature) :
 		self.__organisms[id] = Creature
 
-	def setSunlight(val) :
+	def setSunlight(self, val) :
 		self.__sunlight = val
