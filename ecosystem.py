4109c6ef1b6b2d92ae15bd01ba0f8a67d5c8397d
Nathan Stocking <Nathan@d-p-14-202.eecs.tufts.edu>
2015-11-23 15:28:02 -0500

    fluid movement

--- ecosystem.py
+++ ecosystem.py
@@ -8,6 +8,8 @@ class Ecosystem():
         self.ocean = []
         self.globalTime=0
         self.barrier=Barrier(0)
+        self.hdim = hdim
+        self.vdim = vdim
         for i in range(hdim):
             row = []
             for j in range(vdim):
@@ -22,6 +24,25 @@ class Ecosystem():
             self.barrier.setN(len(self.orgsList)+1)
             self.barrier.wait()
 
+def moveOrganism(self, org, oldLoc, newLoc):
+    #remove from oldLoc
+    while int(newLoc.col < 0): #off west
+        newLoc.col += vdim
+    while int(newLoc.col) >= vdim: #off east
+        newLoc.col -= vdim
+while int(newLoc.row < 0): #off north
+        newLoc.row = 0-newLoc.row
+        newLoc.col = newLoc.col + (hdim/2)
+            if newLoc.col >= vdim:
+            newLoc.col -= vdim
+    while newLoc.row >= hdim: #off south
+        newLoc.row = hdim-newLoc.row #over the pole
+        newLoc.col = newLoc.col + (hdim/2)
+            if newLoc.col >= vdim:
+            newLoc.col -= vdim
+
+
+
     def addOrganism(self, org, loc):
         self.getSeaBlock(loc).addOrganism(org)
         self.orgsList.add(org)
@@ -31,7 +52,7 @@ class Ecosystem():
         self.orgsList.remove(organism)
 
     def getSeaBlock(self, location):
-        return self.ocean[location.row][location.col]
+        return self.ocean[int(location.row)][int(location.col)]
     
     
     def run(self) :
