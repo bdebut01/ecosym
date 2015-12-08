4126cae8391af70dde7ecc03a709b4697f8361b2
bdebut01 <ben@debutts.com>
2015-12-08 11:58:04 -0500

    allow graphics

--- ecosystem.py
+++ ecosystem.py
@@ -173,7 +173,7 @@ class Ecosystem():
             self.barrier.phase1()
             # Print simulation for this tick, could embed this in a if i%amount == 0
             self.printSimulation()
-            #graphic_output.graphicsOutput(self.orgsList, "frame" +str(self.globalTicks) +".jpg", self.hdim, self.vdim)
+            graphic_output.graphicsOutput(self.orgsList, "frame" +str(self.globalTicks) +".jpg", self.hdim, self.vdim)
             
             self.addAndStartNewborns()
             with_lock(self.orgsListMutex, self.endSimulationIfNoOrganisms)
